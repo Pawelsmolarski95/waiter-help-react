240857c7e44c61e104c23df33d90fd6d4faecd7c
Pawelsmolarski95 <109138583+Pawelsmolarski95@users.noreply.github.com>
2022-12-05 08:37:07 +0100

    fix serverproblem

--- server.mjs
+++ server.mjs
@@ -1,14 +1,15 @@
-/* global  */
-import { create, router as _router, defaults, rewriter } from 'json-server';
-const server = create();
-const router = _router('build/db/app.json');
-const middlewares = defaults({
+/* global require, process */
+import jsonServer from 'json-server';
+
+const server = jsonServer.create();
+const router = jsonServer.router('build/db/app.json');
+const middlewares = jsonServer.defaults({
   static: 'build',
   noCors: true
 });
 const port = process.env.PORT || 3131;
 server.use(middlewares);
-server.use(rewriter({
+server.use(jsonServer.rewriter({
   '/api/*': '/$1'
 }));
 
